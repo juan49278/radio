7d1d410d4e24dc174e2264b1a8a551f650123fd4
Juan Martinez <110367048+juan49278@users.noreply.github.com>
2023-07-28 01:02:39 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 addEventListener('DOMContentLoaded', () => {
   showTime()
+  if(localStorage.getItem("visit")== null || localStorage.getItem("visit")== undefined)
+  novedades()
 })
 
-function showTime(){
+addEventListener('click', () =>{
+  clean()
+})
+function showTime() {
   myDate = new Date();
   hours = myDate.getHours();
   minutes = myDate.getMinutes();
@@ -10,6 +15,24 @@ function showTime(){
   if (hours < 10) hours = 0 + hours;
   if (minutes < 10) minutes = "0" + minutes;
   if (seconds < 10) seconds = "0" + seconds;
-  $("#horaActual").text("Hora Actual:" + " " +hours+ ":" +minutes+ ":" +seconds);
+  $("#horaActual").text("Hora Actual:" + " " + hours + ":" + minutes + ":" + seconds);
   setTimeout("showTime()", 1000);
+}
+
+function clean(){
+  let remove = document.querySelector('div.elfsight-app-bc6bfd50-d9ce-4df4-abcc-48d319ce048d').querySelectorAll('a')
+  $(remove).closest("a").remove()
+
+}
+function novedades() {
+  swal({
+    title: "Novedades",
+    text: "Se agregó un Reproductor secundario para poder escuchar con menos uso de datos. Proximamente se agregará una seccion de contacto para que puedas comunicarte o dejarnos tus comentarios, gracias por escuchar Herz FM",
+    icon: "info",
+    button: "Entendido"
+  }).then((result) => {
+    if (result == true) {
+      localStorage.setItem("visit", true)
+    }
+  })
 }
