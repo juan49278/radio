f442315537ad5828d1fe8629df19247c16ac97e1
Juan Martinez <110367048+juan49278@users.noreply.github.com>
2023-08-11 20:19:49 -0300

    add chat beta

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 addEventListener('DOMContentLoaded', () => {
   showTime()
-  if(localStorage.getItem("visit")== null || localStorage.getItem("visit")== undefined)
-  novedades()
+  if (localStorage.getItem("visit") == null || localStorage.getItem("visit") == undefined) {
+    novedades()
+  }
+  if (localStorage.getItem('chats')) {
+    listaComentarios.innerHTML = localStorage.getItem('chats')
+  }
 })
 
 function showTime() {
@@ -27,4 +31,48 @@ function novedades() {
       localStorage.setItem("visit", true)
     }
   })
+}
+
+(() => {
+  'use strict'
+  const forms = document.querySelectorAll('.needs-validation')
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      if (form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        thankyou()
+        publish()
+        wipe()
+        forms.reset()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+function wipe() {
+  document.getElementById("text").value = ""
+  document.getElementById("name").value = ""
+
+}
+function thankyou() {
+  swal({
+    title: "Listo",
+    text: "Gracias por escribirnos",
+    icon: "success"
+  })
+}
+
+function publish() {
+  msg = document.getElementById("text").value
+  nombre = document.getElementById("name").value
+  listaComentarios.innerHTML += `
+  <li class="list-group-sm">${nombre} <span>dijo:</span><br>
+  <p style="font-size:small;">${text.value}</p>`
+  localStorage.setItem('chats', listaComentarios.innerHTML)
 }
